0e1e5e6c1e4c2c457b17ff5cf31de5442bf8931c
Andry1343 <ua.gavrilets@gmail.com>
2023-03-19 23:01:41 +0200

    jimp added

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,10 +4,11 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
- 
- const SECRET_KEY = "33992";
+const Jimp = require("jimp");
 
- const avatarsDir = path.join(__dirname, "../", "public", "avatars");
+const { SECRET_KEY } = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
@@ -22,7 +23,11 @@ const register = async (req, res) => {
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     email: newUser.email,
@@ -74,7 +79,11 @@ const logout = async (req, res) => {
 const subscription = async (req, res) => {
   const { _id } = req.user;
   const { subscription } = req.body;
-  const updatedUser = await User.findByIdAndUpdate(_id, { subscription }, { new: true });
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    { subscription },
+    { new: true }
+  );
   res.status(200).json({
     user: {
       email: updatedUser.email,
@@ -83,19 +92,22 @@ const subscription = async (req, res) => {
   });
 };
 
-const updateAvatar = async(req, res)=> {
-  const {_id} = req.user;
-  const {path: tempUpload, originalname} = req.file;
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
+  const imageAvatar = await Jimp.read(resultUpload);
+  const resizeAvatar = await imageAvatar.resize(250, 250);
+  await resizeAvatar.write(resultUpload);
   const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, {avatarURL});
+  await User.findByIdAndUpdate(_id, { avatarURL });
 
   res.json({
-      avatarURL,
-  })
-}
+    avatarURL,
+  });
+};
 
 module.exports = {
   register: ctrlWrapper(register),
@@ -104,5 +116,4 @@ module.exports = {
   logout: ctrlWrapper(logout),
   subscription: ctrlWrapper(subscription),
   updateAvatar: ctrlWrapper(updateAvatar),
-  
 };
